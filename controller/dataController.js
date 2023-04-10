7e1aeb61a42f05fc140d1010782a007d6bbf1295
Antony Jaison <antonyjaison639@gmail.com>
2023-04-10 22:20:18 +0530

    added random customer numbers

--- controller/dataController.js
+++ controller/dataController.js
@@ -12,37 +12,52 @@ const ProductLine = [
 const PaymentMethod = ["Cash", "Credit card", "Ewallet"];
 
 const dataMaker = async (req, res) => {
+  var randomNum = Math.floor(Math.random() * 10) + 1;
+
+  for(let i=0;i<=randomNum;i++){
+    let randomData = makeRandomData()
+    data.push(randomData)
+  }
+  
+  res.status(200).json(data);
+};
+
+const makeRandomData = () => {
   var Invoice_id = faker.datatype.uuid();
   var Branch = faker.helpers.arrayElement(["A", "B", "c"]);
   var city = faker.helpers.arrayElement(["Naypyitaw", "Mandalay", "Yangon"]);
   var customer_type = faker.helpers.arrayElement(["Member", "Normal"]);
   var Gender = faker.helpers.arrayElement(["Female", "Male"]);
   var Product_line = faker.helpers.arrayElement(ProductLine);
-  var Unit_price = faker.datatype.float({
-    min: 2.29,
-    max: 100.0,
-    precision: 0.01,
-  }).toString()
-  var Quantity = faker.datatype.float({
-    min: 1,
-    max: 10,
-    precision: 1,
-  }).toString()
+  var Unit_price = faker.datatype
+    .float({
+      min: 2.29,
+      max: 100.0,
+      precision: 0.01,
+    })
+    .toString();
+  var Quantity = faker.datatype
+    .float({
+      min: 1,
+      max: 10,
+      precision: 1,
+    })
+    .toString();
   var cogs = (Unit_price * Quantity).toFixed(2);
   var Tax = ((cogs * 20) / 100).toFixed(2);
   var income = Tax;
   var Payment = faker.helpers.arrayElement(PaymentMethod);
   var gross_margin_percent = "4.761904762";
-  var rating = faker.datatype.float({
-    min: 0,
-    max: 10,
-    precision: 0.1,
-  }).toString()
+  var rating = faker.datatype
+    .float({
+      min: 0,
+      max: 10,
+      precision: 0.1,
+    })
+    .toString();
   var Date = "02/18/2021";
   var time = "01:28:00 PM";
   var Total = cogs + Tax;
-
-  var a = [];
 
   var newData = {
     Invoice_id,
@@ -63,10 +78,8 @@ const dataMaker = async (req, res) => {
     time,
     Total,
   };
-    data.push(newData);
-//   a.push(newData)
 
-  res.status(200).json(data);
+  return newData
 };
 
 export { dataMaker };
